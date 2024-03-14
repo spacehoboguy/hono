1db093b5de326a7f628a2dda258e079363667e52
spacehoboguy <ph.olafsen@gmail.com>
2024-03-14 22:28:26 +0100

    buncha stuff

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
 import {Hono} from "hono";
+import {logger} from "hono/logger";
 import {serve} from "@hono/node-server";
 
 const app = new Hono();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 3333;
 
+/**
+ * Middleware
+ */
+app.use(logger());
+
+/**
+ * Routes
+ */
 app.get("/", (c) => c.text("Hono meets node.js spaceguy!"));
 app.get("/hello", (c) => c.text("Hello Hono!"));
 
+/**
+ * Server
+ */
 try {
+
     /**
      * @run "ts-node index.ts"
      * @exit "CTRL+C"
@@ -17,14 +32,13 @@ try {
      */
     serve({
         fetch: app.fetch,
-        port: parseInt(port as string),
+        port: parseInt(port as unknown as string),
     });
-
 } catch (e: unknown) {
+
     console.log(e);
 } finally {
-    const heart = `\uD83D\uDE0D`
 
+    const heart = `\uD83D\uDE0D`
     console.log(`Hono served hot @ http://localhost:${port} ${heart}!`)
-    console.log(`Happy web development!`)
 }
